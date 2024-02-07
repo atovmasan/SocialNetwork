6ddcbd4476b6a3a5f542ddddd1adb0810a53bdf1
artem <yt988998@gmail.com>
2024-02-07 18:29:06 +0300

    asdf

--- SocialNetwork/src/App.tsx
+++ SocialNetwork/src/App.tsx
@@ -2,20 +2,28 @@ import './App.css';
 import {Main} from './Components/Main/Main.tsx';
 import {Profile} from './Components/Profile/Profile.tsx';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from './Redux/Store.ts';
+import {increment} from './Redux/Reducers/PostSlice.ts';
 
 function App() {
-    let count = useSelector((state : RootState) => state.post.value);
+    const dispatch = useDispatch();
+    const count = useSelector((state : RootState) => state.post.value);
+
+    const callback = () => {
+        dispatch(increment())
+    }
+
     return (
         <div className="App">
+            <button onClick={callback}>Increment</button>
+            <p>{count}</p>
             <BrowserRouter>
                 <Routes>
                     <Route path=''
                         element={<Main/>}/>
                     <Route path="profile"
                         element={<Profile/>}/>
-                    <p>{count}</p>
                 </Routes>
             </BrowserRouter>
         </div>
